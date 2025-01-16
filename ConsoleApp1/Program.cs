7dc6f616be52a20072a4bc871a250e7792bf0a68
Ashton Pate <arpate@amarillocollege.com>
2025-01-16 13:21:14 -0600

    updated code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,15 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+           
             // print out the prompt to ask for the name
-            Console.WriteLine("What is your name?")
+            Console.WriteLine("What is your name?");
             // delcare a string variable to hold the use input
             string name = Console.ReadLine();
             // print out the prompt to ask for the fav color
-
             Console.WriteLine("What is your favorite coler?");
             // declare a string varible to hold fav color
             string color = Console.ReadLine();
+            //print out confirmed message
+            Console.WriteLine("Hello " + name + " Your favorite color is " + color);
+            //stop the screen for user to see result
+            Console.ReadLine();
         }
     }
 }
